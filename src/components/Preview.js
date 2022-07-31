601a2af9628ba4c89f48e4d6897255e49f99dd9b
Gabriel Hrastovic <gabrielcro2369@gmail.com>
2022-07-31 10:35:05 -0400

    packagejson removing unnecessary dependencies

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,237 +1,293 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import { ThemeProvider, createTheme, responsiveFontSizes} from '@mui/material/styles';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import parse from 'html-react-parser';
-import Divider from '@mui/material/Divider';
-import Link from '@mui/material/Link'
+import React from "react";
+import Button from "@mui/material/Button";
+import {
+  ThemeProvider,
+  createTheme,
+  responsiveFontSizes,
+} from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import parse from "html-react-parser";
+import Divider from "@mui/material/Divider";
+import Link from "@mui/material/Link";
 
+import "@fontsource/roboto/400.css";
 
-import '@fontsource/roboto/400.css';
-
-
-
-
-const Preview = function({formFields, 
-  formTwoFields, 
-  personalInfo, 
-  projects, 
-  isToggled, setIsToggled, 
-  skills, })
-  
-  {
-
-    
-    
-
- 
+const Preview = function ({
+  formFields,
+  formTwoFields,
+  personalInfo,
+  projects,
+  isToggled,
+  setIsToggled,
+  skills,
+}) {
   let theme = createTheme({
     palette: {
       primary: {
-        main: '#fff',
-        dark: '#35342f',
+        main: "#fff",
+        dark: "#35342f",
       },
       secondary: {
-        main: '#107206',
-        dark: '#fff',
+        main: "#107206",
+        dark: "#fff",
       },
     },
   });
   theme = responsiveFontSizes(theme);
 
-
-    return(
-      
-      <ThemeProvider theme={theme}>
-       
-        <div>
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
         <Container maxWidth="xl">
-
-          
-        {
-            personalInfo.map((personal, index) => (
-              <React.Fragment key={index}>
-                <Box mt={2.5} sx={{display: 'flex', justifyContent: 'center',}}>
-              <Typography variant="h3">{personal.firstname}&nbsp;</Typography>
-              <Typography variant="h3">{personal.lastname}</Typography>
-              </Box>
-              <Box sx={{display: 'flex', justifyContent: 'center'}}>
-              <Typography variant="subtitle1">{personal.title}</Typography>
-              </Box>
-              <Box sx={{display: 'flex', justifyContent: 'center'}}>
-              <Typography variant="subtitle1">{personal.address}</Typography>
-              </Box>
-              
-              <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: {md: '1rem', xs: '0.5rem'} }}>
-              <Typography variant="subtitle2">{personal.phonenumber}</Typography>
-              {personal.email.length > 0 &&
-              <Divider orientation="vertical" flexItem></Divider>
-              }
-              <Typography variant="subtitle2">{personal.email}</Typography>
-              {personal.website.length > 0 &&
-              <Divider orientation="vertical" flexItem></Divider>
-              }
-              <Link href={personal.website} color="#000000" underline="none" variant="subtitle2">{personal.website}</Link>
-              {personal.websitetwo.length > 0 &&
-              <Divider orientation="vertical" flexItem></Divider>
-              }
-              <Link href={personal.websitetwo} color="#000000" underline="none" variant="subtitle2">{personal.websitetwo}</Link>
-              {personal.websitethree.length > 0 &&
-              <Divider orientation="vertical" flexItem></Divider>
-              }
-              <Link href={personal.websitethree} color="#000000" underline="none" variant="subtitle2">{personal.websitethree}</Link>
-              
-              
-              
-              </Box>
-              
-              </React.Fragment>
-              ))
-            }
-
-
-
-{skills.length > 0 && 
-              <React.Fragment>
-              
-
-                 <Typography sx={{ lineHeight: 0.5, fontWeight: 700}} variant="h6" align="left">Skills</Typography>
-                <hr></hr>
-                <Typography ml={5} sx={{ lineHeight: 0.5}} variant="h6">{parse(skills)}</Typography>
-              
-              </React.Fragment>
-  }
-        
-              {projects.length > 0 && 
-              <React.Fragment>
-
-        <Typography variant="h6" align="left" sx={{lineHeight: 0.5, fontWeight: 700}}  > Projects </Typography>
-      
-       <hr></hr>
-       </React.Fragment> 
-      }
-
-{
-            projects.map((project, index) => (
-              
-
+          {personalInfo.map((personal, index) => (
             <React.Fragment key={index}>
-              <Box sx={{display: 'flex'}}>
-              <Typography  ml={5}variant="h6" sx={{lineHeight: 0.5, fontWeight: 700}}>{project.projectTitle}</Typography>
-              <Typography sx={{lineHeight: 0.5, marginLeft: 'auto'}} variant="h6">{project.tech}</Typography>
+              <Box mt={2.5} sx={{ display: "flex", justifyContent: "center" }}>
+                <Typography variant="h3">{personal.firstname}&nbsp;</Typography>
+                <Typography variant="h3">{personal.lastname}</Typography>
               </Box>
-              <Link ml={5} href={project.link} color="#000000" underline="none" sx={{fontStyle: 'italic'}}variant="h6">{project.link}</Link>
-              
-              <Typography  ml={5}  variant="h6">{parse(project.about)}</Typography>
-              
-              
-              
-              </React.Fragment>
-            ))
-          }
+              <Box sx={{ display: "flex", justifyContent: "center" }}>
+                <Typography variant="subtitle1">{personal.title}</Typography>
+              </Box>
+              <Box sx={{ display: "flex", justifyContent: "center" }}>
+                <Typography variant="subtitle1">{personal.address}</Typography>
+              </Box>
 
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: { md: "1rem", xs: "0.5rem" },
+                }}
+              >
+                <Typography variant="subtitle2">
+                  {personal.phonenumber}
+                </Typography>
+                {personal.email.length > 0 && (
+                  <Divider orientation="vertical" flexItem></Divider>
+                )}
+                <Typography variant="subtitle2">{personal.email}</Typography>
+                {personal.website.length > 0 && (
+                  <Divider orientation="vertical" flexItem></Divider>
+                )}
+                <Link
+                  href={personal.website}
+                  color="#000000"
+                  underline="none"
+                  variant="subtitle2"
+                >
+                  {personal.website}
+                </Link>
+                {personal.websitetwo.length > 0 && (
+                  <Divider orientation="vertical" flexItem></Divider>
+                )}
+                <Link
+                  href={personal.websitetwo}
+                  color="#000000"
+                  underline="none"
+                  variant="subtitle2"
+                >
+                  {personal.websitetwo}
+                </Link>
+                {personal.websitethree.length > 0 && (
+                  <Divider orientation="vertical" flexItem></Divider>
+                )}
+                <Link
+                  href={personal.websitethree}
+                  color="#000000"
+                  underline="none"
+                  variant="subtitle2"
+                >
+                  {personal.websitethree}
+                </Link>
+              </Box>
+            </React.Fragment>
+          ))}
 
-{formFields.length > 0 && 
-          <React.Fragment>
-        
-        <Typography sx={{lineHeight: 0.5, fontWeight: 700}} variant="h6" align="left"  >Work Experience </Typography>
-      
+          {skills.length > 0 && (
+            <React.Fragment>
+              <Typography
+                sx={{ lineHeight: 0.5, fontWeight: 700 }}
+                variant="h6"
+                align="left"
+              >
+                Skills
+              </Typography>
               <hr></hr>
-        
-         </React.Fragment>     
-  }    
+              <Typography ml={5} sx={{ lineHeight: 0.5 }} variant="h6">
+                {parse(skills)}
+              </Typography>
+            </React.Fragment>
+          )}
 
+          {projects.length > 0 && (
+            <React.Fragment>
+              <Typography
+                variant="h6"
+                align="left"
+                sx={{ lineHeight: 0.5, fontWeight: 700 }}
+              >
+                {" "}
+                Projects{" "}
+              </Typography>
 
-              
-        {
-            formFields.map((formField, index) => (
-              
-              
-            <React.Fragment key={index}>
-              
-              <Box sx={{display: 'flex'}}>
-              <Typography ml={5} sx={{lineHeight: 0.5, fontWeight: 700}}variant="h6">{formField.employer}</Typography>
-              <Typography sx={{lineHeight: 0.5, marginLeft: 'auto'}} variant="h6">{formField.location}</Typography>
-              </Box>
-              <Box sx={{display: 'flex'}}>
-              <Typography ml={5} sx={{lineHeight: 1.5, fontStyle: 'italic'}}variant="h6">{formField.occupation}</Typography>
-              <Typography sx={{lineHeight: 1.5, marginLeft: 'auto'}} variant="h6">{formField.startdate}&nbsp;-&nbsp;{formField.enddate}</Typography>
-              </Box>
-
-              
-              
-              <Typography ml={5} variant="h6">{parse(formField.duties)}</Typography>
-              
-              
-              
-              </React.Fragment>
-              
-            ))
-          }
-        
-
-
-        {formTwoFields.length > 0 && 
-          <React.Fragment>
-        
-        <Typography sx={{lineHeight: 0.5, fontWeight: 700}}variant="h6" align="left"  > Education </Typography>
-      
               <hr></hr>
-        
-         </React.Fragment>     
-  }    
+            </React.Fragment>
+          )}
 
-
-
-
-
-        {
-            formTwoFields.map((formTwoField, index) => (
-
+          {projects.map((project, index) => (
             <React.Fragment key={index}>
-              <Box sx={{display: 'flex'}}>
-              <Typography ml={5} sx={{lineHeight: 0.5, fontWeight: 700}} variant="h6">{formTwoField.school}</Typography>
-              <Typography sx={{lineHeight: 0.5, marginLeft: 'auto'}} variant="h6">{formTwoField.location}</Typography>
+              <Box sx={{ display: "flex" }}>
+                <Typography
+                  ml={5}
+                  variant="h6"
+                  sx={{ lineHeight: 0.5, fontWeight: 700 }}
+                >
+                  {project.projectTitle}
+                </Typography>
+                <Typography
+                  sx={{ lineHeight: 0.5, marginLeft: "auto" }}
+                  variant="h6"
+                >
+                  {project.tech}
+                </Typography>
               </Box>
-              <Box sx={{display: 'flex'}}>
-              <Typography ml={5} sx={{lineHeight: 1.5, }} variant="h6">{formTwoField.certification}</Typography>
-              <Typography sx={{lineHeight: 1.5, marginLeft: 'auto'}} variant="h6">{formTwoField.startdate}&nbsp;-&nbsp;{formTwoField.enddate}</Typography>
+              <Link
+                ml={5}
+                href={project.link}
+                color="#000000"
+                underline="none"
+                sx={{ fontStyle: "italic" }}
+                variant="h6"
+              >
+                {project.link}
+              </Link>
+
+              <Typography ml={5} variant="h6">
+                {parse(project.about)}
+              </Typography>
+            </React.Fragment>
+          ))}
+
+          {formFields.length > 0 && (
+            <React.Fragment>
+              <Typography
+                sx={{ lineHeight: 0.5, fontWeight: 700 }}
+                variant="h6"
+                align="left"
+              >
+                Work Experience{" "}
+              </Typography>
+
+              <hr></hr>
+            </React.Fragment>
+          )}
+
+          {formFields.map((formField, index) => (
+            <React.Fragment key={index}>
+              <Box sx={{ display: "flex" }}>
+                <Typography
+                  ml={5}
+                  sx={{ lineHeight: 0.5, fontWeight: 700 }}
+                  variant="h6"
+                >
+                  {formField.employer}
+                </Typography>
+                <Typography
+                  sx={{ lineHeight: 0.5, marginLeft: "auto" }}
+                  variant="h6"
+                >
+                  {formField.location}
+                </Typography>
               </Box>
-              
-              
-              
-              
-              </React.Fragment>
-            ))
-          }
+              <Box sx={{ display: "flex" }}>
+                <Typography
+                  ml={5}
+                  sx={{ lineHeight: 1.5, fontStyle: "italic" }}
+                  variant="h6"
+                >
+                  {formField.occupation}
+                </Typography>
+                <Typography
+                  sx={{ lineHeight: 1.5, marginLeft: "auto" }}
+                  variant="h6"
+                >
+                  {formField.startdate}&nbsp;-&nbsp;{formField.enddate}
+                </Typography>
+              </Box>
 
+              <Typography ml={5} variant="h6">
+                {parse(formField.duties)}
+              </Typography>
+            </React.Fragment>
+          ))}
 
-      </Container>
-        </div>
-        <Box className="noprint" sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-        <Button color="secondary" variant="outlined" onClick={() => setIsToggled(isToggled)}>Back to editing</Button>
-          <Button color="secondary" variant="outlined" onClick={ window.print}>
-        Download resume as PDF
-      </Button>
+          {formTwoFields.length > 0 && (
+            <React.Fragment>
+              <Typography
+                sx={{ lineHeight: 0.5, fontWeight: 700 }}
+                variant="h6"
+                align="left"
+              >
+                {" "}
+                Education{" "}
+              </Typography>
+
+              <hr></hr>
+            </React.Fragment>
+          )}
+
+          {formTwoFields.map((formTwoField, index) => (
+            <React.Fragment key={index}>
+              <Box sx={{ display: "flex" }}>
+                <Typography
+                  ml={5}
+                  sx={{ lineHeight: 0.5, fontWeight: 700 }}
+                  variant="h6"
+                >
+                  {formTwoField.school}
+                </Typography>
+                <Typography
+                  sx={{ lineHeight: 0.5, marginLeft: "auto" }}
+                  variant="h6"
+                >
+                  {formTwoField.location}
+                </Typography>
+              </Box>
+              <Box sx={{ display: "flex" }}>
+                <Typography ml={5} sx={{ lineHeight: 1.5 }} variant="h6">
+                  {formTwoField.certification}
+                </Typography>
+                <Typography
+                  sx={{ lineHeight: 1.5, marginLeft: "auto" }}
+                  variant="h6"
+                >
+                  {formTwoField.startdate}&nbsp;-&nbsp;{formTwoField.enddate}
+                </Typography>
+              </Box>
+            </React.Fragment>
+          ))}
+        </Container>
+      </div>
+      <Box
+        className="noprint"
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <Button
+          color="secondary"
+          variant="outlined"
+          onClick={() => setIsToggled(isToggled)}
+        >
+          Back to editing
+        </Button>
+        <Button color="secondary" variant="outlined" onClick={window.print}>
+          Download resume as PDF
+        </Button>
       </Box>
-        </ThemeProvider>
-        
-        
-      
+    </ThemeProvider>
+  );
+};
 
-
-     
-        
-        
-       
-
-
-        
-        
-        
-    )
-}
-
-export default Preview
+export default Preview;
